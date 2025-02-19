59f314237e36489420e81225dc8ef152368e6108
nyel-101 <dandevjr@gmail.com>
2025-02-19 19:39:52 +0000

    chore(worker): fix caching layer

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21aedfd7d4b54539b5901886eff67d5d = () => {
-  return '2026-02-03T22:53:38Z';
+const node_7b3b8e0c466247159ff503417c3c1c4c = () => {
+  return '2025-02-19T19:39:52Z';
 };
