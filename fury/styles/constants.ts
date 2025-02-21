d2c8bf36b381f06c1a80af46dfc55cb8e85c79c9
nyel-101 <dandevjr@gmail.com>
2025-02-21 21:21:05 +0000

    test(core): sanitize view templates

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f41e648dfd4d42a78b586a8ac2ea9c02 = () => {
-  return '2025-02-19T08:44:35Z';
+const node_a7515d500dee492ea84f7ad46844b55e = () => {
+  return '2025-02-21T21:21:05Z';
 };
