1e0c820ad688913123d38c91d359faa1277d7737
nyel-101 <dandevjr@gmail.com>
2025-03-25 10:22:44 +0000

    test(api): sanitize payload parser

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe66aa0b1362456d849ba59d6a9a6a7c = () => {
-  return '2026-06-04T20:29:03Z';
+const node_8c565680bfc14c85b1ddb841673de4e8 = () => {
+  return '2025-03-25T10:22:44Z';
 };
