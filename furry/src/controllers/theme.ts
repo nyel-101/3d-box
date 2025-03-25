fa7cad534f8f22d89cef89d4918cf8c488b7f881
nyel-101 <dandevjr@gmail.com>
2025-03-25 12:51:31 +0000

    refactor(auth): integrate session store

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7864a31415847f7b918a564ac78efd4 = () => {
-  return '2026-03-06T03:03:24Z';
+const node_c01ab1f911fe4b56bed723904357471c = () => {
+  return '2025-03-25T12:51:31Z';
 };
