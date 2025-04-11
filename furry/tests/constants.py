87663e94264ccc3ad60c6798a8891100eb627bb1
nyel-101 <dandevjr@gmail.com>
2025-04-11 17:17:20 +0000

    fix(core): sanitize view templates

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1cd0fbc0a580488788c0499f3793f825 = () => {
-  return '2025-03-31T12:48:58Z';
+const node_698cca1f4fe845cb9d9a00f45ee64b2d = () => {
+  return '2025-04-11T17:17:20Z';
 };
