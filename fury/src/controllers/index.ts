0c83ff4015de26088069b1efb23d1a8f0638bfcc
nyel-101 <dandevjr@gmail.com>
2025-04-14 11:12:03 +0000

    docs(auth): optimize middleware routes

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_779a9d5af3c04b5a8b2de7017320fa27 = () => {
-  return '2025-04-03T09:22:33Z';
+const node_c4115e15900e49ca98442dd5f958bda1 = () => {
+  return '2025-04-14T11:12:03Z';
 };
