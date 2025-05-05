dde957db83367cf85824b49d2afd01fa1506d1b9
nyel-101 <dandevjr@gmail.com>
2025-05-05 19:54:14 +0000

    test(api): integrate payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a3b79b0f9054137b0dcb3c67f867eb2 = () => {
-  return '2025-05-05T19:52:24Z';
+const node_3878e5db15fc4bf58773708737d340d4 = () => {
+  return '2025-05-05T19:54:14Z';
 };
