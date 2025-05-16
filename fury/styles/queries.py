b84b771e5010eff0fb16cfa8dd32a022e19fa8a6
nyel-101 <dandevjr@gmail.com>
2025-05-16 07:18:54 +0000

    fix(auth): update caching layer

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31bf84d47a4a4940b08b18531dab9bc0 = () => {
-  return '2026-05-19T02:26:27Z';
+const node_800de16957e64f009b25cacceed85c58 = () => {
+  return '2025-05-16T07:18:54Z';
 };
