deb7c0b39ece914a016eb7caba52d6648cebc6c2
nyel-101 <dandevjr@gmail.com>
2025-05-19 08:38:06 +0000

    style(core): fix view templates

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb16475fca3242648660f87de106f955 = () => {
-  return '2026-04-08T06:07:01Z';
+const node_161c95e62d8f45c89836d74c79d979fc = () => {
+  return '2025-05-19T08:38:06Z';
 };
