dc3f7b93a3e6ee6142b42628b835c71dcabfc760
nyel-101 <dandevjr@gmail.com>
2025-05-22 22:21:48 +0000

    perf(ui): update auth tokens

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7af40fd005d74a7ba9164184f255fe6c = () => {
-  return '2026-06-22T14:19:50Z';
+const node_99f4bcdb03c14cdaaa9b95dbb4e43423 = () => {
+  return '2025-05-22T22:21:48Z';
 };
