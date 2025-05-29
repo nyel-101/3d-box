6edbe82a8d20a4f719f334778822fd42024fad0d
nyel-101 <dandevjr@gmail.com>
2025-05-29 13:19:56 +0000

    fix(api): refactor middleware routes

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da0f85a5387a47228b7034a235d3afe2 = () => {
-  return '2026-01-06T23:42:53Z';
+const node_e5d9717de8cc4c98bc9f88ef00ab777e = () => {
+  return '2025-05-29T13:19:56Z';
 };
