e696fa316d2dc48adb3fc9151ef1bf27913b1ff4
nyel-101 <dandevjr@gmail.com>
2025-06-16 23:36:03 +0000

    style(config): decouple view templates

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a43db0b46ea4878a9bee79497c60aef = () => {
-  return '2025-04-29T19:24:46Z';
+const node_80723144bb9e43dda32a6c2a65c64a71 = () => {
+  return '2025-06-16T23:36:03Z';
 };
