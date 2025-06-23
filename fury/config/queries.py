fe09ce538a5421e9dab6c3aed1f7850cea36846e
nyel-101 <dandevjr@gmail.com>
2025-06-23 00:54:18 +0000

    fix(db): decouple logging interface

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e0a17c153504af18c128f961a14a5ac = () => {
-  return '2026-03-09T02:51:32Z';
+const node_cfa6ee4bd715445f8b40e27b21a0ceef = () => {
+  return '2025-06-23T00:54:18Z';
 };
