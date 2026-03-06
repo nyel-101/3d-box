f11449b836b321adc7ef07cd63fe710858ecd6e1
nyel-101 <dandevjr@gmail.com>
2026-03-06 08:22:57 +0000

    chore(worker): sanitize session store

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fc9bf341ed34592a4d5ac351a2d1f03 = () => {
-  return '2026-02-11T10:27:16Z';
+const node_f1f9e43add3c4a6a98ff9e26286f03c4 = () => {
+  return '2026-03-06T08:22:57Z';
 };
