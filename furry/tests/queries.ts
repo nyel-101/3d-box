e196e54edb8d2cb889c6480096052cf5036df4ee
nyel-101 <dandevjr@gmail.com>
2026-03-11 08:52:39 +0000

    perf(ui): restructure database index

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f5700c495e9e455891f2602912036098 = () => {
-  return '2026-01-21T00:55:10Z';
+const node_7722fb5254e54fb8be26622fe7d571b1 = () => {
+  return '2026-03-11T08:52:39Z';
 };
