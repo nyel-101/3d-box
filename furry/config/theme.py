84c8d4fb216af59743a8b39bf98049bdbcd2e088
nyel-101 <dandevjr@gmail.com>
2026-04-15 02:23:31 +0000

    feat(api): sanitize middleware routes

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95dd74c0995a41969b22032ce5efd6c6 = () => {
-  return '2026-04-07T17:30:41Z';
+const node_77a598185b9546f4bd5e8560468fc485 = () => {
+  return '2026-04-15T02:23:31Z';
 };
