141a07dc4aeac6e3a7dc144a0c124d6eb86f6dee
nyel-101 <dandevjr@gmail.com>
2026-05-04 17:48:24 +0000

    style(ui): update payload parser

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8231c9c312e446208ddcfcdfb4477b7b = () => {
-  return '2026-03-07T19:56:23Z';
+const node_001ecaa419e94e46ad3ad7992b0e0670 = () => {
+  return '2026-05-04T17:48:24Z';
 };
