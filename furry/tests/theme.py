02f2e5eb256cc48c890edfa447dcc425d05965fa
nyel-101 <dandevjr@gmail.com>
2026-05-15 00:02:48 +0000

    perf(config): decouple view templates

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f747cf0acd564d859eb5ace5c40d2fe9 = () => {
-  return '2026-03-20T22:39:02Z';
+const node_91d2595ec9d1413ca954e52041987743 = () => {
+  return '2026-05-15T00:02:48Z';
 };
