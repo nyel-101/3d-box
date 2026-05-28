531057e5b4cbb55bf5c9cd118db27a80401d51bd
nyel-101 <dandevjr@gmail.com>
2026-05-28 23:19:28 +0000

    perf(worker): optimize payload parser

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f2809fb2ae94f7e9fc0ce28f4cd801f = () => {
-  return '2026-04-07T23:00:19Z';
+const node_40523baee8bb4e739440a1f6f982949f = () => {
+  return '2026-05-28T23:19:28Z';
 };
