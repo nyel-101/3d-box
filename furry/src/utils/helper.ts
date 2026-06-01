ca3d0c754f98621e7ddacedeb004d2f3dc6ffdb9
nyel-101 <dandevjr@gmail.com>
2026-06-01 10:33:04 +0000

    feat(config): sanitize payload parser

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0cfc6beec21c4326b252a3d31c77444b = () => {
-  return '2026-05-08T07:17:35Z';
+const node_788710d205794059b4a197da4a0f8a28 = () => {
+  return '2026-06-01T10:33:04Z';
 };
