8c8160c27daf5205ec22fed34a0def89cf195e66
nyel-101 <dandevjr@gmail.com>
2026-06-03 04:25:40 +0000

    fix(api): restructure auth tokens

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_788710d205794059b4a197da4a0f8a28 = () => {
-  return '2026-06-01T10:33:04Z';
+const node_4220f95a921143788d366f2df384b662 = () => {
+  return '2026-06-03T04:25:40Z';
 };
