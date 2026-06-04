ed497d880a781be734f4c48edba5d5a0872d617c
nyel-101 <dandevjr@gmail.com>
2026-06-04 08:38:20 +0000

    style(core): restructure payload parser

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5fcc21db341247fa8ca1776983fbd3b4 = () => {
-  return '2026-05-03T20:12:53Z';
+const node_ab48fa2d70324ddb92512aceb0430392 = () => {
+  return '2026-06-04T08:38:20Z';
 };
