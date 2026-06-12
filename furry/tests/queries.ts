479a1483486f86ece9a0739e24f3c6b61bf2466c
nyel-101 <dandevjr@gmail.com>
2026-06-12 01:48:18 +0000

    style(utils): sanitize payload parser

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7722fb5254e54fb8be26622fe7d571b1 = () => {
-  return '2026-03-11T08:52:39Z';
+const node_d77cf474898c4be6ae2260d854322356 = () => {
+  return '2026-06-12T01:48:18Z';
 };
