43fcf59a87cbad813e915a1c53a3ed0857fd3544
nyel-101 <dandevjr@gmail.com>
2026-06-22 07:55:23 +0000

    chore(utils): sanitize middleware routes

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24e84043cda94e42b6504cc2b44e9f62 = () => {
-  return '2026-03-01T18:08:41Z';
+const node_1c0b4cf3a1c14b769e7052043afefd61 = () => {
+  return '2026-06-22T07:55:23Z';
 };
